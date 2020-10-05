de8763c874667e305efea2e68f57da2deb04a8d9
vblan <vblan@DESKTOP-91V5R8E>
2020-10-05 09:08:30 +0200

    Leer y escribir ficheros camiones DAO

--- Práctica 3/src/Persistencia/CamionDao.java
+++ Práctica 3/src/Persistencia/CamionDao.java
@@ -1,15 +1,8 @@
 package Persistencia;
 
 import Dominio.*;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 
 public class CamionDao extends VehiculoDao  {
@@ -17,6 +10,55 @@ public class CamionDao extends VehiculoDao  {
 	public CamionDao() {
 		
 	}
+	
+	public ArrayList<Camion> leerCamiones() throws IOException {
+		ArrayList<Camion> camiones = new ArrayList<Camion>();
+		Scanner in = new Scanner(new FileReader("Camiones.txt"));
+		in.next();
+
+		int contador = in.nextInt();
+
+		for (int i = 0; i < contador; i++) {
+			in.next();
+			String matricula = in.nextLine();
+			in.next();
+			String marca = in.nextLine();
+			in.next();
+			String modelo = in.next();
+			in.next();
+			String color = in.next();
+			in.next();
+			double precio = in.nextDouble();
+			in.next();
+			int capacidadcarga = in.nextInt();
+			Camion ca = new Camion(matricula, marca, modelo, color, precio, capacidadcarga);
+			camiones.add(ca);
+		}
+
+		return camiones;
+	
+}
+	
+	public void escribirCamiones(ArrayList<Camion> camiones) throws IOException {
+		PrintWriter out = new PrintWriter(new FileWriter("Camiones.txt"));
+		out.println("Camion: ");
+		out.println(camiones.size());
+		
+		for (int i = 0; i < camiones.size(); i++) {
+			out.println("Matricula: ");
+			out.println(camiones.get(i).getMatricula());
+			out.println("Marca: ");
+			out.println(camiones.get(i).getMarca());
+			out.println("Modelo: ");
+			out.println(camiones.get(i).getModelo());
+			out.println("Color: ");
+			out.println(camiones.get(i).getColor());
+			out.println("Precio: ");
+			out.println(camiones.get(i).getPrecio());
+			out.println("Capacidad de carga: ");
+			out.println(camiones.get(i).getCapacidadcarga());
+}
+		out.close();}
 	
 	public boolean insertar(Vehiculo camiones) throws ClassNotFoundException {
 		boolean registrar = false;
